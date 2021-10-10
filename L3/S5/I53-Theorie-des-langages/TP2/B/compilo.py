3edd7a0cd62a989bda1f432f2833ff5f25a0177c
Rakuzan05 <75131398+Rakuzan05@users.noreply.github.com>
2021-10-10 19:34:49 +0200

    Update compilo.py
    André : Same que pour A

--- L3/S5/I53-Theorie-des-langages/TP2/B/compilo.py
+++ L3/S5/I53-Theorie-des-langages/TP2/B/compilo.py
@@ -1,18 +1,18 @@
-import Scanner, Parser, Codegen, sys, os
+import Scanner, Parser, Codegen, sys, os    #Appel des modules précédents
 
-if (os.path.isfile(sys.argv[1])):
+if (os.path.isfile(sys.argv[1])):           #Cas d'un fichier
     f = open(sys.argv[1],"r")
     str_s = f.read()
 else:
-    str_s = sys.argv[1]
+    str_s = sys.argv[1]                     #Cas d'une chaîne de caractères
 
-l = Scanner.scanner(str_s)
+l = Scanner.scanner(str_s)                  #Analyse lexicale
 
 if l:
-    postfix = Parser.parser(l)
+    postfix = Parser.parser(l)              #Analyse syntaxique
 else:
     exit(1)
 
-if postfix:
+if postfix:                                 #Simulation de la pile + passage en postfixe
     Codegen.codegen(postfix)
     os.system("chmod u+x a.out")
